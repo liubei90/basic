eea5579e354dbe014f3bedd98ae19164508f1c81
1021086660@qq.com <1021086660@qq.com>
2021-02-25 07:46:48 +0800

    2-25

--- wxminiprogram/demo_packages/miniprogram/sub_package_authorize/pages/authsetting/authsetting.js
+++ wxminiprogram/demo_packages/miniprogram/sub_package_authorize/pages/authsetting/authsetting.js
@@ -5,6 +5,10 @@ Page({
   onLoad() {
   },
   onShow() {
+    this.consoleSetting();
+  },
+
+  consoleSetting() {
     wx.getSetting({
       withSubscriptions: true,
       success(res) {
@@ -13,9 +17,68 @@ Page({
       fail(err) {
         console.dir(err);
       }
-    })
+    });
   },
-
+  openSetting() {
+    wx.openSetting({
+      withSubscriptions: true,
+      success(res) {
+        console.dir(res);
+      },
+      fail(err) {
+        console.dir(err);
+      }
+    });
+  },
+  doAuth(scope, authFun) {
+    wx.getSetting({
+      withSubscriptions: true,
+      success: (res) => {
+        console.dir(res);
+        // 未进行过授权
+        if (res.authSetting[scope] === undefined) {
+          authFun();
+        } 
+        // 进行授权时被拒绝
+        else if (res.authSetting[scope] === false) {
+          this.openSetting();
+          // wx.openSetting({
+          //   withSubscriptions: true,
+          //   success: (res) => {
+          //     console.dir(res);
+          //     wx.getSetting({
+          //       withSubscriptions: true,
+          //       success: (res_2) => {
+          //         // 设置面板授权失败， 提示授权失败
+          //         if (res_2.authSetting[scope] === false) {
+          //           ;
+          //         } 
+          //         // 授权成功
+          //         else {
+          //           authFun();
+          //         }
+          //       },
+          //       fail: (err) => {
+          //         ;
+          //       }
+          //     })
+          //   },
+          //   fail: (err) => {
+          //     // 打开设置面板失败，做授权失败提示
+          //     console.dir(err);
+          //   }
+          // });
+        } 
+        // 授权成功
+        else if (res.authSetting[scope] === true) {
+          authFun();
+        }
+      },
+      fail(err) {
+        console.dir(err);
+      }
+    });
+  },
   authorize(e) {
     wx.authorize({
       scope: e.currentTarget.dataset.scope,
@@ -24,8 +87,15 @@ Page({
       },
       fail(err) {
         console.error(err);
+      },
+      complete: () => {
+        this.consoleSetting();
       }
     });
+  },
+  onGetUserInfoByOpenTypeBtn(e) {
+    console.dir(e);
+    this.consoleSetting();
   },
   onGetUserInfo() {
     wx.getUserInfo({
@@ -35,16 +105,24 @@ Page({
       fail(err) {
         console.error(err);
       },
+      complete: () => {
+        this.consoleSetting();
+      }
     });
   },
   onGetLocation() {
-    wx.getLocation({
-      success(res) {
-        console.dir(res);
-      },
-      fail(err) {
-        console.error(err);
-      },
+    this.doAuth('scope.userLocation', () => {
+      wx.getLocation({
+        success(res) {
+          console.dir(res);
+        },
+        fail(err) {
+          console.error(err);
+        },
+        complete: () => {
+          this.consoleSetting();
+        }
+      });
     });
   },
   onChooseLocation() {
@@ -55,6 +133,9 @@ Page({
       fail(err) {
         console.error(err);
       },
+      complete: () => {
+        this.consoleSetting();
+      }
     });
   },
 })
