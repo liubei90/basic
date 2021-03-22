69abff114607bc67884af2a52d1b1f63047601b0
1021086660@qq.com <1021086660@qq.com>
2021-03-22 22:06:55 +0800

    3-22

--- wxminiprogram/demo_service/customerServiceMessage/receiver.py
+++ wxminiprogram/demo_service/customerServiceMessage/receiver.py
@@ -1,10 +1,28 @@
 import json
 import uuid
+import hashlib
 from tornado.web import RequestHandler
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPResponse
 
+token='100861'
+
+def encode_msg(timestamp, nonce):
+    arr = [token, timestamp, nonce]
+    arr.sort()
+    arrstr = ''.join(arr)
+
+    return hashlib.sha1(arrstr.encode('utf8')).hexdigest()
+
+
 class CustomerServiceMessageReceiver(RequestHandler):
     async def get(self):
-        print(self.request.arguments)
-        print(self.request.headers)
-        print(self.request.body)
+        signature = self.get_argument('signature', None)
+        timestamp = self.get_argument('timestamp', None)
+        nonce = self.get_argument('nonce', None)
+        echostr = self.get_argument('echostr', None)
+
+        print(signature)
+        print(encode_msg(timestamp, nonce))
+
+        if signature == encode_msg(timestamp, nonce):
+            return self.write(echostr)
